2ea70ac8234ac8d3042d5e84bd733ef8bee935d7
Jacob Schrauder <jacobschrauder10@gmail.com>
2023-03-28 18:38:07 -0400

    added histogram equalization to script

--- JACart_video_loop_emotion_detection.py
+++ JACart_video_loop_emotion_detection.py
@@ -6,13 +6,14 @@ import datetime
 analyzedFramesList = []
 
 # number of frames between emotion detection tests
-frameInterval = 225
+frameInterval = 15
 
 # load video file - short
 # vidFile = cv2.VideoCapture('E:/jmuSpring2023/IT445/newTestSpring_2023-02-14-10-21-00__passenger_cam_passenger_left.mp4')
 
 # load video file - long
-vidFile = cv2.VideoCapture('E:/jmuFall2022/Capstone/bahaa_2022-11-17-14-21-26__passenger_cam_passenger_left.mp4')
+#vidFile = cv2.VideoCapture('E:/jmuFall2022/Capstone/emotionDetectionTest_2023-03-08-17-20-57__passenger_cam_passenger_left.mp4')
+vidFile = cv2.VideoCapture('E:/jmuSpring2023/IT445/baselineTestShort_2023-03-28-17-29-33__passenger_cam_passenger_left.mp4')
 if (vidFile.isOpened() == False):
     print("error opening the video file")
 else:
@@ -28,9 +29,14 @@ while vidFile.isOpened():
         if vidFile.get(1) % frameInterval == 0:
             analyzedFrame = frame[:, :, ::-1]
 
+            # Uncomment the next 3 lines for histogram equalization
+            grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            equalizedFrame = cv2.equalizeHist(grayFrame)
+            processedFrame = cv2.cvtColor(equalizedFrame, cv2.COLOR_GRAY2BGR)
             # Analyze the frame
 
-            result = DeepFace.analyze(analyzedFrame, actions=['emotion'])
+            # Change analyzedFrame to processedFrame for histogram equalization
+            result = DeepFace.analyze(processedFrame, actions=['emotion'])
 
             # Use the following line when you just want the dominant emotion
             dominantEmotion = max(result['emotion'].items(), key=lambda x: x[1])[0]
@@ -39,7 +45,7 @@ while vidFile.isOpened():
             timestamp = str(datetime.timedelta(milliseconds=timestamp_ms))
 
             # Use the following line when you want all the emotion values
-            # analyzedFramesList.append(f"Timestamp of video: {timestamp}  Result: {result}")
+            #analyzedFramesList.append(f"Timestamp of video: {timestamp}  Result: {result}")
 
             analyzedFramesList.append(f"Timestamp of video: {timestamp}  Result: {dominantEmotion}")
     except ValueError:
@@ -51,6 +57,6 @@ vidFile.release()
 
 print(analyzedFramesList)
 
-with open(r'E:/jmuSpring2023/IT445/VideoDetectionOutput/test1_Long_Video_Dominant.txt', 'w') as fp:
+with open(r'E:/jmuSpring2023/IT445/VideoDetectionOutput/inside-baselineTest_dominant_emotion.txt', 'w') as fp:
     for entry in analyzedFramesList:
         fp.write("%s\n" % entry)
